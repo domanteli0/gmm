c068903972655ac5e3eb58b6df088df1881bd0a9
Domantelio✨ <domantelio@proton.me>
2024-04-09 10:29:48 +0300

    lab2: attempt #6

--- lab2/trans2.py
+++ lab2/trans2.py
@@ -5,13 +5,13 @@ train_trans = trans.Compose([
   trans.RandomRotation(20),
   trans.ColorJitter(brightness = 0.4, contrast = 0.2, saturation = 0.2, hue=0.1),
   trans.RandomGrayscale(p=0.1),
-  trans.RandomResizedCrop(size = (32, 32), scale = (0.75, 0.75)),
+  trans.RandomResizedCrop(size = (64, 64), scale = (0.75, 0.75)),
   trans.ToTensor(),
   trans.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
 validation_trans = trans.Compose([
-  trans.Resize((32, 32)),
+  trans.Resize((64, 64)),
   trans.ToTensor(),
   trans.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
